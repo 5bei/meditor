db2bf577c474d929e2d67a160cad9e011067b7a1
5bei <hundred134@gmail.com>
2017-11-09 14:49:50 +0700

    代码提交

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import { bodyAppend } from './utils/dom'
-import tplWrap from './tpl'
-import core from './core'
-
-import './index.less'
-
-const jmcaculator = function() {
-	bodyAppend(tplWrap)
-
-	core()
-}
-
-module.exports = jmcaculator
-
-jmcaculator()
 
 
+document.write('heiheihei')
